dbc7f717dd2cd1b746b97a53aff2308748a0c544
yasothamohankumar <133649359+yasothamohankumar@users.noreply.github.com>
2023-05-17 12:17:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-zmkdkg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-zmkdkg)
---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+
+import './app.css'
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export  function App() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+
